32cbc60c26312edf454338e89557717fd81959ec
Sophia Jin <sophiejin28@gmail.com>
2024-06-06 13:54:57 -0700

    updated resource view and added green pools to lvl1

--- Levels/level_3.gd
+++ Levels/level_3.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("CanvasLayer/stone/Label").text = str(GameState.stones_mined) + "/15"
-	get_node("CanvasLayer/iron/Label").text = str(GameState.stones_mined) + "/10"
+	get_node("CanvasLayer/iron/Label").text = str(GameState.steel_mined) + "/10"
 	fire_sound.play()
 
 
